0f017e25169d7efe714fae6ab964610952bfb66f
Sapana2402 <sapna.prajapati2001@gmail.com>
2025-06-26 14:31:19 +0530

    New app

--- ExpenseTracker/Sources/Extension/UIView+Inspectable.swift
+++ ExpenseTracker/Sources/Extension/UIView+Inspectable.swift
@@ -5,4 +5,31 @@
 //  Created by MACM26 on 26/06/25.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get { layer.cornerRadius }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get { layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let cgColor = layer.borderColor else { return nil }
+            return UIColor(cgColor: cgColor)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
